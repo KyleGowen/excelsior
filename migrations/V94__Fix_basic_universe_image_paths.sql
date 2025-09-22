21278d5986bc92fd6cd4f1fa70da8e9d1307f504
KyleGowen <kyle@measurabl.com>
2025-09-22 04:17:53 +0000

    Add sorting to basic universe cards in database view
    - Sort by type using OverPower order (Energy, Combat, Brute Force, Intelligence, Any-Power)
    - Secondary sort by value_to_use (numeric)
    - Tertiary sort by bonus (numeric)
    - Cards now display in logical order within each type group

---
+++ migrations/V94__Fix_basic_universe_image_paths.sql
@@ -0,0 +1,13 @@
+-- Fix basic universe card image paths to match correct filenames
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_energy_2.webp' WHERE name = 'Ray Gun';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_energy_3.webp' WHERE name = 'Merlin''s Wand';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/7_energy_3.webp' WHERE name = 'Lightning Bolt';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_combat_2.webp' WHERE name = 'Flintlock';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_combat_3.webp' WHERE name = 'Rapier';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/7_combat_3.webp' WHERE name = 'Longbow';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_brute_force_2.webp' WHERE name = 'Hyde''s Serum';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_brute_force_3.webp' WHERE name = 'Trident';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/7_brute_force_3.webp' WHERE name = 'Trebuchet';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_intelligence_2.webp' WHERE name = 'Secret Identity';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/6_intelligence_3.webp' WHERE name = 'Advanced Technology';
+UPDATE basic_universe_cards SET image_path = 'basic-universe/7_intelligence_3.webp' WHERE name = 'Magic Spell';
